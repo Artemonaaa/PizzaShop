672f2090478d0c0253eb1e757aebf91637c12db8
Artemonaaa <artemidaabc@gmail.com>
2023-08-19 23:08:28 +0300

    Add commponent Pizza

--- src/app/components/PizzaDetails.js
+++ src/app/components/PizzaDetails.js
@@ -1,5 +1,22 @@
-const PizzaDetails = () => {
-  return <div>PizzaDetails</div>;
+import React, { useState, useEffect } from 'react'
+
+import Image from 'next/image'
+
+import SizeSelection from './SizeSelection'
+import CrustSelection from './CrustSelection'
+import Topping from './Topping'
+
+const PizzaDetails = ({ pizza, modal, setModal }) => {
+  const [size, setSize] = useState("")
+  const [crust, setCrust] = useState("")
+  const [additionalTopping, setAdditionalTopping] = useState("")
+  const [additionalToppingPrice, setAdditionalToppingPrice] = useState("")
+  const [price, setPrice] = useState(0)
+  
+
+  return (
+    <div></div>
+  )
 };
 
 export default PizzaDetails;
