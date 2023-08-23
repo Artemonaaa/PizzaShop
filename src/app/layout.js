ed74c0bdb7950cac3c709d9ea62e7ab9807c1ae7
Artemonaaa <artemidaabc@gmail.com>
2023-08-23 20:51:57 +0300

    Add component Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Nav from './components/Nav';
+
 import './globals.css';
 
 import { Bangers, Quicksand, Roboto_Condensed } from 'next/font/google'
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
       <body 
         className={`${quicksand.variable} ${bangers.variable} ${robotoCondensed.variable} font-quicksand`}
       >
+        <Nav />
         {children}
       </body>
     </html>
